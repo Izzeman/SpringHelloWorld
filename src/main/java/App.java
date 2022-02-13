70e6bf44a29f3d29acf9a17fc32b7ba019866a12
Denis Petiev <denisap@inbox.ru>
2022-02-13 16:43:26 +0300

    add Cat.class add catBean to AppConfig made comparison of beans in Main

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+
+        HelloWorld first =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld second =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat tom = (Cat) applicationContext.getBean("cat");
+        Cat felix = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(first.equals(second));
+        System.out.println(tom.equals(felix));
     }
 }
